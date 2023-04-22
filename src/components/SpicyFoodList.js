11168b60559d52cfaa384bdd2ff93db98fb4eefd
Ronald Barasa <barasaogoro@gmail.com>
2023-04-22 20:40:45 +0300

    code along

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,14 +6,58 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood]
+    setFoods(newFoodArray);
   }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
+  const foodList = foodsToDisplay.map((food) => (
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
+
+  function handleClick(id){
+    const newFoodArray = foods.map ((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+
+    setFoods(newFoodArray);
+  }
+  
+  
+
+  const foodsToDisplay = foods.filter((food) => {
+    if (filterBy === "All") {
+      return true;
+    } else {
+      return food.cuisine === filterBy;
+    }
+    
+  });
+
+  const [filterBy, setFilterBy] = useState("All")
+  const [foods, setFoods] = useState(spicyFoods);
+  function handleFilterChange(event){
+    setFilterBy(event.target.value);
+  }
+  return (
+    <select name="filter" onChange={handleFilterChange}>
+      <option value="All">All</option>
+      <option value="American">American</option>
+      <option value="Sichuan">Sichuan</option>
+      <option value="Thai">Thai</option>
+      <option value="Mexican">Mexican</option>
+    </select>
+  );
+}
+
 
   return (
     <div>
@@ -21,6 +65,5 @@ function SpicyFoodList() {
       <ul>{foodList}</ul>
     </div>
   );
-}
 
 export default SpicyFoodList;
